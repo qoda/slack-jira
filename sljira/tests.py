01f88dc4e850dcdb7df2869e3327ea558cee08a4
qoda <jpbydendyk@gmail.com>
2016-04-19 20:59:58 +0200

    Updated tests

--- sljira/tests.py
+++ sljira/tests.py
@@ -12,14 +12,17 @@ class DBTestCase(unittest.TestCase):
 
     def test_keyvalstore(self):
         self.keyvalstore.set(self.key, self.value)
-        value = self.keyvalstore.get(self.key)
 
         # Ensure the entry is save and can be accessed again
-        self.assertEqual(self.value, value)
+        self.assertEqual(self.value, self.keyvalstore.get(self.key))
 
         # Ensure the no exception is raised if a value can not be found and
         # none is returned
         self.assertEqual(self.keyvalstore.get("fake_key"), None)
+
+        # Ensure values can be updated
+        self.keyvalstore.set(self.key, "new_value")
+        self.assertEqual(self.keyvalstore.get(self.key), "new_value")
 
     def tearDown(self):
         os.remove(os.path.join(os.path.dirname(__file__), "../", "test.db"))
